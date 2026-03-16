59e113520e8b272e436d9e2bd07d407fa0ea7980
Zeticuz <zeticuz@nexus.app>
2026-03-16 15:28:13 +0800

    Migrate from gsap-trial to standard gsap package (gsap is now free)

--- src/gsap-trial.d.ts
+++ src/gsap-trial.d.ts
@@ -1,6 +1,2 @@
-declare module 'gsap-trial/ScrollSmoother' {
-  export const ScrollSmoother: any;
-}
-declare module 'gsap-trial/SplitText' {
-  export const SplitText: any;
-}
+// These modules are now part of the standard 'gsap' package (free since April 2025)
+// No longer need module declarations as they come built-in with gsap
